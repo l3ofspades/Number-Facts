182125f9e53eec33862cb0843c892c49db9495d3
L3ofspades <jonathanfederico13@gmail.com>
2025-04-13 10:09:19 -0700

    Number Facts
    Javascript Promises- Asynchronous Code  Part 1 Number Facts

---
+++ main.js
@@ -0,0 +1,58 @@
+const factsDiv = 
+document.getElementById("facts");
+console.log("factsDiv", factsDiv);
+// Check if the div is found
+
+// Single fact
+function getSingleFact() {
+    console.log("fetching single fact");
+    // confirms function runs
+    fetch("http://numbersapi.com/13?json")
+    .then(res => {
+        console.log("Got response", res);
+        // Check if the response is ok
+         res.json();
+    })
+    .then(data => {
+        console.log("Parsed data", data);
+        //Check what the api returns
+        const p =
+            document.createElement("p");
+            p.textContent = "single fact: " + data.text;
+            factsDiv.appendChild(p); 
+    })
+    .catch(err => {
+        console.log("Fetch error", err);
+        // Catch and show any errors
+    });
+
+}
+
+// Multiple facts
+function getMultipleFacts() {
+    fetch("http://numbersapi.com/13?json&count=3")
+    .then(res => res.json())
+    .then(data => {
+        const p =
+            document.createElement("p");
+            p.textContent = "multiple facts: " + data.text;
+            factsDiv.appendChild(p); 
+    });   
+}
+
+// Four facts about the same number
+function getFourFacts() {
+    fetch("http://numbersapi.com/13?json&count=4")
+    .then(res => res.json())
+    .then(data => {
+        const p =
+            document.createElement("p");
+            p.textContent = "four facts: " + data.text;
+            factsDiv.appendChild(p); 
+    });   
+}
+
+// Call the functions 
+getSingleFact();
+getMultipleFacts();
+getFourFacts();
